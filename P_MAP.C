e824186c50bf706c426236224f2c9b01d5ee4491
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2017-03-14 19:15:34 -0400

    Don't let items block Lost Souls
    https://doomwiki.org/wiki/Lost_soul_colliding_with_items

--- P_MAP.C
+++ P_MAP.C
@@ -270,13 +270,6 @@ boolean PIT_CheckThing (mobj_t* thing)
 	damage = ((P_Random()%8)+1)*tmthing->info->damage;
 	
 	P_DamageMobj (thing, tmthing, tmthing, damage);
-	
-	tmthing->flags &= ~MF_SKULLFLY;
-	tmthing->momx = tmthing->momy = tmthing->momz = 0;
-	
-	P_SetMobjState (tmthing, tmthing->info->spawnstate);
-	
-	return false;		// stop moving
     }
 
     
