35fd277d2e02d934431367e2655596b8079dc002
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2017-03-20 19:59:06 -0400

    Fix missiles exploding against the sky
    https://doomwiki.org/wiki/Projectiles_explode_on_impact_with_%22sky%22

--- P_MOBJ.C
+++ P_MOBJ.C
@@ -334,7 +334,14 @@ void P_ZMovement (mobj_t* mo)
 	if ( (mo->flags & MF_MISSILE)
 	     && !(mo->flags & MF_NOCLIP) )
 	{
-	    P_ExplodeMissile (mo);
+	    if (mo->subsector->sector->ceilingpic == skyflatnum)
+	    {
+	        P_RemoveMobj (mo);
+	    }
+	    else
+	    {
+	        P_ExplodeMissile (mo);
+	    }
 	    return;
 	}
     }
